e9b7f8faae5ae479b1a659ce5f24bac7c6d8594d
bhaskar3 <bhaskar3@node-0.bhaskar3-295354.cc-profiler-pg0.utah.cloudlab.us>
2026-04-09 22:18:11 -0600

    Add a better utility comparison function for PCC

--- scripts/ovld_configs/pcc_config.go
+++ scripts/ovld_configs/pcc_config.go
@@ -1,12 +1,14 @@
 package pcc
 
+import "math"
+
 // Server config
 const (
-	SpccQdelayBudget              uint64  = 55
+	SpccQdelayBudget              uint64  = 250
 	SpccRttUs                     uint64  = 10
-  	SpccPreMonIntUs               uint64  = 10
-  	SpccMonIntUs                  uint64  = 10
-  	SpccEpsilon                   uint64  = 10
+  	SpccPreMonIntUs               uint64  = 200
+  	SpccMonIntUs                  uint64  = 500
+  	SpccEpsilon                   uint64  = 1
     SpccMicroExpStrictLabelling   bool = false
     SpccMicroExpPerturbCp         bool = true
 )
@@ -18,6 +20,16 @@ func SpccCalcUtilFn(stats *SpccMicroExpStats) float64 {
 
 // Utility comparison function
 func SpccCompUtilFn(minusStats, plusStats *SpccMicroExpStats) SpccDirType {
+
+	var utilDiff float64
+	var utilDiffPcnt float64
+
+	utilDiff = math.Abs(plusStats.Utility - minusStats.Utility)
+	utilDiffPcnt = utilDiff / minusStats.Utility
+
+	if utilDiffPcnt < 0.1 {
+		return SpccDirStay
+	}
 	if plusStats.Utility > minusStats.Utility {
 		return SpccDirPlus
 	}
